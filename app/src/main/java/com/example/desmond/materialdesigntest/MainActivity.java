d6ce076d4318932dd43418c2085d8cd9e9f1f162
desmond.agyeman <dizzy6teen@yahoo.com>
2015-03-26 20:25:57 +0000

    testing commit

--- app/src/main/java/com/example/desmond/materialdesigntest/MainActivity.java
+++ app/src/main/java/com/example/desmond/materialdesigntest/MainActivity.java
@@ -4,14 +4,17 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //todo tìmake a new commit
     }
 
     @Override
