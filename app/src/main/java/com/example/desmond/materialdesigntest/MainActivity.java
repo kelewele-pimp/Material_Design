2e857b551435cec83becd979b040448920ee626d
desmond.agyeman <dizzy6teen@yahoo.com>
2015-03-26 20:28:26 +0000

    commit: Setting up App theme and colors

--- app/src/main/java/com/example/desmond/materialdesigntest/MainActivity.java
+++ app/src/main/java/com/example/desmond/materialdesigntest/MainActivity.java
@@ -4,12 +4,10 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 
 public class MainActivity extends ActionBarActivity {
 
-    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
