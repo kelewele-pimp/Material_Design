7e3c6c867e30a4b2a2ba5d4e1b01f2660b7539a1
desmond.agyeman <dizzy6teen@yahoo.com>
2015-04-01 17:57:21 +0100

    setting up navigation drawer

--- app/src/main/java/com/example/desmond/materialdesigntest/MainActivity.java
+++ app/src/main/java/com/example/desmond/materialdesigntest/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.desmond.materialdesigntest;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
@@ -12,6 +13,7 @@ import android.widget.Toast;
 public class MainActivity extends ActionBarActivity {
 
     private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +21,11 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+        //Set up the navigation drawer
+        NavigationDrawerFragment navigationDrawerFragment =
+                (NavigationDrawerFragment) getFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        navigationDrawerFragment.setup(drawerLayout, toolbar);
     }
 
     @Override
